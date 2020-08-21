f8388706ecee37c6a7adc1975f71f8a3bdecb800
Mr. R <31630644+Rumpelstiltsquin@users.noreply.github.com>
2020-08-21 10:56:31 -0400

    Update CheckWeb.py

--- CheckWeb.py
+++ CheckWeb.py
@@ -2,21 +2,23 @@ import sys
 from urllib.request import Request, urlopen
 from urllib.error import URLError, HTTPError
 
-if len(sys.argv)==2:
+if len(sys.argv)==3:
     target = Request(sys.argv[1])
-    try:
-        response = urlopen(target)
-    except HTTPError as e:
-        print('The server couldn\'t fulfill the request.')
-        print('Error code: ', e.code)
-    except URLError as e:
-        print('failed to reach a server.')
-        print('Reason: ', e.reason)
-    else:
-        print ('Website is working fine')
+    threat = int (sys.argv[2])
+    for t in range(threat):
+        try:
+            response = urlopen(target)
+        except HTTPError as e:
+            print('The server couldn\'t fulfill the request.')
+            print('Error code: ', e.code)
+        except URLError as e:
+            print('failed to reach a server.')
+            print('Reason: ', e.reason)
+        else:
+            print ('Website is working fine')
 else:
     print("""
     HELP!
     Givme an argument, Example:
-    #python3 CheckWeb.py httpe://www.any-site.com
+    #python3 CheckWeb.py https://www.any-site.com threats [int]
     """)
